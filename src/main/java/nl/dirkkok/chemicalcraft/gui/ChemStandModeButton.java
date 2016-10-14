6facaacb1a576cb3b6eba1f5c821d863ee9f9548
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-14 23:02:16 +0200

    custom button with icon

---
+++ src/main/java/nl/dirkkok/chemicalcraft/gui/ChemStandModeButton.java
@@ -0,0 +1,31 @@
+package nl.dirkkok.chemicalcraft.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.util.ResourceLocation;
+import nl.dirkkok.chemicalcraft.tileentity.ChemistryStandEntity;
+
+class ChemStandModeButton extends GuiButton {
+	private ChemistryStandEntity te;
+	private GuiChemistryStandEntity gui;
+	
+	ChemStandModeButton(int id, int posX, int posY, GuiChemistryStandEntity gui) {
+		super(id, posX, posY, 20, 20, "");
+		this.te = gui.te;
+		this.gui = gui;
+	}
+	
+	public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+		super.drawButton(mc, mouseX, mouseY);
+		
+		mc.getTextureManager().bindTexture(new ResourceLocation("chemicalcraft:textures/gui/chemical_stand.png"));
+		switch (this.te.getMode()) {
+			case 0: this.drawTexturedModalRect(gui.i + 30, gui.j + 50, 176, 71,  20, 20);
+				break;
+			case 1: this.drawTexturedModalRect(gui.i + 30, gui.j + 50, 176, 91,  20, 20);
+				break;
+			case 2: this.drawTexturedModalRect(gui.i + 30, gui.j + 50, 176, 111, 20, 20);
+				break;
+		}
+	}
+}
