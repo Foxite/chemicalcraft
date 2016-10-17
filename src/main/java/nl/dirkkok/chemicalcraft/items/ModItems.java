2440c926e4e016558a53206b9d37eb23226574c5
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-17 17:38:57 +0200

    added table salt

--- src/main/java/nl/dirkkok/chemicalcraft/items/ModItems.java
+++ src/main/java/nl/dirkkok/chemicalcraft/items/ModItems.java
@@ -3,10 +3,13 @@ package nl.dirkkok.chemicalcraft.items;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 
 public final class ModItems {
 	public static Item testTube;
 	public static Item residueTray;
+	public static Item tableSalt;
+	public static Item saltpeterDust;
 	
 	public static final CreativeTabs chemTab = new CreativeTabs("chemicalCraft") {
 		@Override
@@ -14,9 +17,18 @@ public final class ModItems {
 			return testTube;
 		}
 	};
-
+	
 	public static void createItems() {
 		GameRegistry.register(testTube = new TestTube());
 		GameRegistry.register(residueTray = new BasicItem("residue_tray"));
+		GameRegistry.register(tableSalt = new BasicItem("table_salt"));
+		GameRegistry.register(saltpeterDust = new BasicItem("saltpeter_dust"));
+		
+		// OreDictionary
+		OreDictionary.registerOre("foodSalt", tableSalt);
+		OreDictionary.registerOre("dustSalt", tableSalt);
+		
+		OreDictionary.registerOre("dustSaltpeter", saltpeterDust);
+		
 	}
 }
