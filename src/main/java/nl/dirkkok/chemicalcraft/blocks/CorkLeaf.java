c94c1bdba43dcbbdf7da4a2a7a9d8edf15ce804f
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-25 16:08:07 +0200

    now extends BlockLeaves

--- src/main/java/nl/dirkkok/chemicalcraft/blocks/CorkLeaf.java
+++ src/main/java/nl/dirkkok/chemicalcraft/blocks/CorkLeaf.java
@@ -1,12 +1,12 @@
 package nl.dirkkok.chemicalcraft.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
@@ -22,157 +22,22 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class CorkLeaf extends BasicBlock implements net.minecraftforge.common.IShearable {
+public class CorkLeaf extends BlockLeaves implements net.minecraftforge.common.IShearable {
 	public static final PropertyBool DECAYABLE = PropertyBool.create("decayable");
 	public static final PropertyBool CHECK_DECAY = PropertyBool.create("check_decay");
-	private boolean leavesFancy;
-	private int[] surroundings;
+	int[] surroundings;
 	
 	public CorkLeaf() {
-		super("cork_leaf", Material.LEAVES, 0.2F, 1F);
+		//super("cork_leaf", Material.LEAVES, 0.2F, 1F);
+		this.setRegistryName("cork_leaf");
+		this.setUnlocalizedName("cork_leaf");
 		this.setTickRandomly(true);
 		this.setSoundType(SoundType.PLANT);
-		this.setDefaultState(this.blockState.getBaseState().withProperty(CHECK_DECAY, Boolean.TRUE).withProperty(DECAYABLE, Boolean.TRUE));
-	}
-	
-	
-	@Override
-	public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
-	{
-		int k = pos.getX();
-		int l = pos.getY();
-		int i1 = pos.getZ();
-		
-		if (worldIn.isAreaLoaded(new BlockPos(k - 2, l - 2, i1 - 2), new BlockPos(k + 2, l + 2, i1 + 2)))
-		{
-			for (int j1 = -1; j1 <= 1; ++j1)
-			{
-				for (int k1 = -1; k1 <= 1; ++k1)
-				{
-					for (int l1 = -1; l1 <= 1; ++l1)
-					{
-						BlockPos blockpos = pos.add(j1, k1, l1);
-						IBlockState iblockstate = worldIn.getBlockState(blockpos);
-						
-						if (iblockstate.getBlock().isLeaves(iblockstate, worldIn, blockpos))
-						{
-							iblockstate.getBlock().beginLeavesDecay(iblockstate, worldIn, blockpos);
-						}
-					}
-				}
-			}
-		}
+		this.setDefaultState(this.blockState.getBaseState().withProperty(CHECK_DECAY, Boolean.TRUE)
+				.withProperty(DECAYABLE, Boolean.TRUE));
 	}
 	
 	@Override
-	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
-	{
-		if (!worldIn.isRemote)
-		{
-			if (state.getValue(CHECK_DECAY) && state.getValue(DECAYABLE))
-			{
-				int k = pos.getX();
-				int l = pos.getY();
-				int i1 = pos.getZ();
-				
-				if (this.surroundings == null)
-				{
-					this.surroundings = new int[32768];
-				}
-				
-				if (worldIn.isAreaLoaded(new BlockPos(k - 5, l - 5, i1 - 5), new BlockPos(k + 5, l + 5, i1 + 5)))
-				{
-					BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
-					
-					for (int i2 = -4; i2 <= 4; ++i2)
-					{
-						for (int j2 = -4; j2 <= 4; ++j2)
-						{
-							for (int k2 = -4; k2 <= 4; ++k2)
-							{
-								IBlockState iblockstate = worldIn.getBlockState(blockpos$mutableblockpos.setPos(k + i2, l + j2, i1 + k2));
-								Block block = iblockstate.getBlock();
-								
-								if (!block.canSustainLeaves(iblockstate, worldIn, blockpos$mutableblockpos.setPos(k + i2, l + j2, i1 + k2)))
-								{
-									if (block.isLeaves(iblockstate, worldIn, blockpos$mutableblockpos.setPos(k + i2, l + j2, i1 + k2)))
-									{
-										this.surroundings[(i2 + 16) * 1024 + (j2 + 16) * 32 + k2 + 16] = -2;
-									}
-									else
-									{
-										this.surroundings[(i2 + 16) * 1024 + (j2 + 16) * 32 + k2 + 16] = -1;
-									}
-								}
-								else
-								{
-									this.surroundings[(i2 + 16) * 1024 + (j2 + 16) * 32 + k2 + 16] = 0;
-								}
-							}
-						}
-					}
-					
-					for (int i3 = 1; i3 <= 4; ++i3)
-					{
-						for (int j3 = -4; j3 <= 4; ++j3)
-						{
-							for (int k3 = -4; k3 <= 4; ++k3)
-							{
-								for (int l3 = -4; l3 <= 4; ++l3)
-								{
-									if (this.surroundings[(j3 + 16) * 1024 + (k3 + 16) * 32 + l3 + 16] == i3 - 1)
-									{
-										if (this.surroundings[(j3 + 16 - 1) * 1024 + (k3 + 16) * 32 + l3 + 16] == -2)
-										{
-											this.surroundings[(j3 + 16 - 1) * 1024 + (k3 + 16) * 32 + l3 + 16] = i3;
-										}
-										
-										if (this.surroundings[(j3 + 16 + 1) * 1024 + (k3 + 16) * 32 + l3 + 16] == -2)
-										{
-											this.surroundings[(j3 + 16 + 1) * 1024 + (k3 + 16) * 32 + l3 + 16] = i3;
-										}
-										
-										if (this.surroundings[(j3 + 16) * 1024 + (k3 + 16 - 1) * 32 + l3 + 16] == -2)
-										{
-											this.surroundings[(j3 + 16) * 1024 + (k3 + 16 - 1) * 32 + l3 + 16] = i3;
-										}
-										
-										if (this.surroundings[(j3 + 16) * 1024 + (k3 + 16 + 1) * 32 + l3 + 16] == -2)
-										{
-											this.surroundings[(j3 + 16) * 1024 + (k3 + 16 + 1) * 32 + l3 + 16] = i3;
-										}
-										
-										if (this.surroundings[(j3 + 16) * 1024 + (k3 + 16) * 32 + (l3 + 16 - 1)] == -2)
-										{
-											this.surroundings[(j3 + 16) * 1024 + (k3 + 16) * 32 + (l3 + 16 - 1)] = i3;
-										}
-										
-										if (this.surroundings[(j3 + 16) * 1024 + (k3 + 16) * 32 + l3 + 16 + 1] == -2)
-										{
-											this.surroundings[(j3 + 16) * 1024 + (k3 + 16) * 32 + l3 + 16 + 1] = i3;
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-				
-				int l2 = this.surroundings[16912];
-				
-				if (l2 >= 0)
-				{
-					worldIn.setBlockState(pos, state.withProperty(CHECK_DECAY, Boolean.FALSE), 4);
-				}
-				else
-				{
-					this.dropBlockAsItem(worldIn, pos, worldIn.getBlockState(pos), 0);
-					worldIn.setBlockToAir(pos);
-				}
-			}
-		}
-	}
-	
 	@SideOnly(Side.CLIENT)
 	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
 	{
@@ -198,37 +63,23 @@ public class CorkLeaf extends BasicBlock implements net.minecraftforge.common.IS
 		return ModBlocks.corkSaplingItem;
 	}
 	
-	/*
+	@Override
 	public boolean isOpaqueCube(IBlockState state)
 	{
-		return !this.leavesFancy;
-	}*/
-	
-	@SideOnly(Side.CLIENT)
-	public void setGraphicsLevel(boolean fancy)
-	{
-		this.leavesFancy = fancy;
+		return !Minecraft.isFancyGraphicsEnabled();
 	}
 	
 	@SideOnly(Side.CLIENT) @Override
 	public BlockRenderLayer getBlockLayer()
 	{
-		return this.leavesFancy ? BlockRenderLayer.CUTOUT_MIPPED : BlockRenderLayer.SOLID;
+		return Minecraft.isFancyGraphicsEnabled() ? BlockRenderLayer.CUTOUT_MIPPED : BlockRenderLayer.SOLID;
 	}
+	
 	
 	@Override
 	public boolean isVisuallyOpaque()
 	{
-		return false;
-	}
-	
-	@Override public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos){ return true; }
-	@Override public boolean isLeaves(IBlockState state, IBlockAccess world, BlockPos pos){ return true; }
-	
-	@Override
-	public List<ItemStack> onSheared(ItemStack item, net.minecraft.world.IBlockAccess world, BlockPos pos, int fortune)
-	{
-		return Collections.singletonList(new ItemStack(this, 1));
+		return !Minecraft.isFancyGraphicsEnabled();
 	}
 	
 	@Override
@@ -259,9 +110,20 @@ public class CorkLeaf extends BasicBlock implements net.minecraftforge.common.IS
 		return ret;
 	}
 	
+	@Override public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos){ return true; }
+	
+	@Override public boolean isLeaves(IBlockState state, IBlockAccess world, BlockPos pos){ return true; }
+	
+	@Override
+	public List<ItemStack> onSheared(ItemStack item, net.minecraft.world.IBlockAccess world, BlockPos pos, int fortune)
+	{
+		return Collections.singletonList(new ItemStack(this, 1));
+	}
+	
+	@Override
 	protected BlockStateContainer createBlockState()
 	{
-		return new BlockStateContainer(this, new IProperty[] {CHECK_DECAY, DECAYABLE});
+		return new BlockStateContainer(this, CHECK_DECAY, DECAYABLE);
 	}
 	
 	@Override
@@ -280,11 +142,16 @@ public class CorkLeaf extends BasicBlock implements net.minecraftforge.common.IS
 				.withProperty(DECAYABLE, state.getValue(DECAYABLE));
 	}
 	
-	/*
+	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
 	{
-		return !(!this.leavesFancy && blockAccess.getBlockState(pos.offset(side)).getBlock() == this) && super.shouldSideBeRendered(blockState, blockAccess, pos, side);
-	}*/
+		return !(!Minecraft.isFancyGraphicsEnabled() && blockAccess.getBlockState(pos.offset(side)).getBlock() == this) && super.shouldSideBeRendered(blockState, blockAccess, pos, side);
+	}
+	
+	@Override
+	public BlockPlanks.EnumType getWoodType(int i) {
+		return BlockPlanks.EnumType.OAK;
+	}
 	
 }
