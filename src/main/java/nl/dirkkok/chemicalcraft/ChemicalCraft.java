dd1bcbc65320bb1b7ae0404c7f9c6e685af0f6e5
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-25 16:28:56 +0200

    added support for Railcraft, and added several debugging log calls

--- src/main/java/nl/dirkkok/chemicalcraft/ChemicalCraft.java
+++ src/main/java/nl/dirkkok/chemicalcraft/ChemicalCraft.java
@@ -1,5 +1,7 @@
 package nl.dirkkok.chemicalcraft;
 
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.LoaderState;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -7,8 +9,10 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod(modid = ChemicalCraft.MODID, name = ChemicalCraft.MODNAME, version = ChemicalCraft.VERSION)
 @SuppressWarnings("unused")
@@ -16,27 +20,50 @@ public class ChemicalCraft {
 	public static final String MODID = "chemicalcraft";
 	public static final String MODNAME = "ChemicalCraft";
 	public static final String VERSION = "0.1";
-	public static final Logger LOG = LogManager.getLogger();
-
+	public static Logger LOG;
 	@SidedProxy(clientSide = "nl.dirkkok.chemicalcraft.ClientProxy",
 				serverSide = "nl.dirkkok.chemicalcraft.ServerProxy")
 	public static CommonProxy proxy;
-
 	@Instance
 	public static ChemicalCraft instance = new ChemicalCraft();
+	
+	// List of loaded mods that this mod supports. Used internally.
+	public static final List<String> supportedModsLoaded = new ArrayList<String>();
+	private boolean errorHasBeenLogged = false;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
+		LOG = e.getModLog();
+		
 		proxy.preInit(e);
 		LOG.info("This is " + MODNAME + " version " + VERSION);
+		if (e.getModState() == LoaderState.ModState.ERRORED) {
+			LOG.error(MODNAME + " preinitialization FAILED");
+			errorHasBeenLogged = true;
+		}
+		
 	}
 
 	@EventHandler
-	public void init(FMLInitializationEvent e) { proxy.init(e); }
+	public void init(FMLInitializationEvent e) {
+		proxy.init(e);
+		if (e.getModState() == LoaderState.ModState.ERRORED && !errorHasBeenLogged) {
+			LOG.error(MODNAME + " initialization FAILED");
+			errorHasBeenLogged = true;
+		}
+	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e) {
+		if (Loader.isModLoaded("railcraft")) {
+			supportedModsLoaded.add("railcraft");
+		}
+		
 		proxy.postInit(e);
-		LOG.info(MODNAME + " initialization successful");
+		if (e.getModState() == LoaderState.ModState.ERRORED && !errorHasBeenLogged) {
+			LOG.error(MODNAME + " postinitialization FAILED");
+		} else {
+			LOG.info(MODNAME + " initialization successful");
+		}
 	}
 }
