afdac5594bf866e5b1603822fdf30e3eaa7cb38d
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-27 20:19:53 +0200

    added 20 new test tube contents

--- src/main/java/nl/dirkkok/chemicalcraft/items/TestTube.java
+++ src/main/java/nl/dirkkok/chemicalcraft/items/TestTube.java
@@ -12,16 +12,37 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import nl.dirkkok.chemicalcraft.ChemicalCraft;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Contract;
 
 import java.util.List;
 
 /* Metadata:
- * 0: Empty
- * 1: Fresh water
- * 2: Pure water
- * 3: NaCl
+ * 0:  Empty
+ * 1:  Fresh water
+ * 2:  Pure water
+ * 3:  NaCl
+ * 4:  Nitric acid
+ * 5:  Sulfuric acid
+ * 6:  Mixture of nitric and sulfuric acid
+ * 7:  Toluene
+ * 8:  Nitrotoluene
+ * 9:  Sodium bicarbonate
+ * 10: Cleaned nitrotoluene
+ * 11: Unfinished TNT
+ * 12: Raw TNT
+ * 13: Copper(II) nitrate
+ * 14: Sodium nitrate
+ * 15: Ammonia (stopped only)
+ * 16: Salty water
+ * 17: Mixture of ammonia and salty water
+ * 18: CO2 (stopped only)
+ * 19: Mixture of ammonia, salty water and CO2
+ * 20: Elemental hydrogen (stopped only)
+ * 21: Elemental nitrogen (stopped only)
+ * 22: Elemental oxygen (stopped only)
  *
  * For stopped tubes, the first bit is 1. For unstopped tubes, the first bit is 0. Tubes with gas content do not have
  * unstopped contents registered.
@@ -38,8 +59,30 @@ class TestTube extends BasicItem {
 			case 0: type = ""; break;
 			case 1: type = "_water"; break;
 			case 2: type = "_purewater"; break;
-			case 3: type = "_nacl"; break;
-			default: type = ""; break;
+			case 3: type = "_table_salt"; break;
+			case 4: type = "_nitric_acid"; break;
+			case 5: type = "_sulfuric_acid"; break;
+			case 6: type = "_nitric+sulfuric_acid"; break;
+			case 7: type = "_toluene"; break;
+			case 8: type = "_nitrotoluene"; break;
+			case 9: type = "_sodium_bicarbonate"; break;
+			case 10: type = "_cleaned_nitrotoluene"; break;
+			case 11: type = "_unfinished_tnt"; break;
+			case 12: type = "_raw_tnt"; break;
+			case 13: type = "_copper2_nitrate"; break;
+			case 14: type = "_sodium_nitrate"; break;
+			case 15: type = "_ammonia"; break;
+			case 16: type = "_salty_water"; break;
+			case 17: type = "_ammonia+salty_water"; break;
+			case 18: type = "_co2"; break;
+			case 19: type = "_ammonia+salty_water+co2"; break;
+			case 20: type = "_hydrogen"; break;
+			case 21: type = "_nitrogen"; break;
+			case 22: type = "_oxygen"; break;
+			default: type = "";
+					 ChemicalCraft.LOG.error("Unrecognized metadata " + setBit(stack.getItemDamage(), 30, false) + " " +
+							 "with " + (getBit(stack.getItemDamage(), 30) == 1 ? "" : "no") + " stopper");
+					 break;
 		}
 		
 		if (getBit(stack.getItemDamage(), 30) == 1) type = type + "_stopped";
@@ -49,17 +92,58 @@ class TestTube extends BasicItem {
 	
 	@Override
 	public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
-		// Unstopped tubes (with supporting contents)
-		subItems.add(new ItemStack(itemIn, 1, 0)); // Empty
-		subItems.add(new ItemStack(itemIn, 1, 1)); // Water
-		subItems.add(new ItemStack(itemIn, 1, 2)); // Pure water
-		subItems.add(new ItemStack(itemIn, 1, 3)); // NaCl
+		/* Gas contents and thus only in stopped tubes:
+		 * Ammonia
+		 * CO2
+		 * Elemental hydrogen
+		 * Elemental nitrogen
+		 * Elemental oxygen
+		 */
+		
+		// Unstopped tubes
+		subItems.add(new ItemStack(itemIn, 1,  0)); // Empty
+		subItems.add(new ItemStack(itemIn, 1,  1)); // Water (l)
+		subItems.add(new ItemStack(itemIn, 1,  2)); // Pure water (l)
+		subItems.add(new ItemStack(itemIn, 1,  3)); // NaCl (s)
+		subItems.add(new ItemStack(itemIn, 1,  4)); // Nitric acid (l)
+		subItems.add(new ItemStack(itemIn, 1,  5)); // Sulfuric acid (l)
+		subItems.add(new ItemStack(itemIn, 1,  6)); // Mixture of nitric and sulfuric acid (l)
+		subItems.add(new ItemStack(itemIn, 1,  7)); // Toluene (l)
+		subItems.add(new ItemStack(itemIn, 1,  8)); // Nitrotoluene (l
+		subItems.add(new ItemStack(itemIn, 1,  9)); // Sodium bicarbonate (s)
+		subItems.add(new ItemStack(itemIn, 1, 10)); // Cleaned nitrotoluene (l)
+		subItems.add(new ItemStack(itemIn, 1, 11)); // Unfinished TNT (s)
+		subItems.add(new ItemStack(itemIn, 1, 12)); // Raw TNT (s)
+		subItems.add(new ItemStack(itemIn, 1, 13)); // Copper(II) nitrate (s)
+		subItems.add(new ItemStack(itemIn, 1, 14)); // Sodium nitrate (s)
+		subItems.add(new ItemStack(itemIn, 1, 16)); // Salty water (l)
+		subItems.add(new ItemStack(itemIn, 1, 17)); // Mixture of ammonia and salty water (l)
+		subItems.add(new ItemStack(itemIn, 1, 19)); // Mixture of ammonia, salty water and CO2 (l)
 		
 		// Stopped tubes
-		subItems.add(new ItemStack(itemIn, 1, setBit(0, 30, true))); // Empty
-		subItems.add(new ItemStack(itemIn, 1, setBit(1, 30, true))); // Water
-		subItems.add(new ItemStack(itemIn, 1, setBit(2, 30, true))); // Pure water
-		subItems.add(new ItemStack(itemIn, 1, setBit(3, 30, true))); // NaCl
+		subItems.add(new ItemStack(itemIn, 1, setBit(0, 30, true))); // Empty (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(1, 30, true))); // Water (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(2, 30, true))); // Pure water (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(3, 30, true))); // NaCl (s)
+		subItems.add(new ItemStack(itemIn, 1, setBit(4, 30, true))); // Nitric acid (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(5, 30, true))); // Sulfuric acid (l) Technically solid below 10C, but whatever
+		subItems.add(new ItemStack(itemIn, 1, setBit(6, 30, true))); // Mixture of nitric and sulfuric acid (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(7, 30, true))); // Toluene (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(8, 30, true))); // Nitrotoluene (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(9, 30, true))); // Sodium bicarbonate (s)
+		subItems.add(new ItemStack(itemIn, 1, setBit(10, 30, true))); // Cleaned nitrotoluene (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(11, 30, true))); // Unfinished TNT (s)
+		subItems.add(new ItemStack(itemIn, 1, setBit(12, 30, true))); // Raw TNT (s)
+		subItems.add(new ItemStack(itemIn, 1, setBit(13, 30, true))); // Copper(II) nitrate (s)
+		subItems.add(new ItemStack(itemIn, 1, setBit(14, 30, true))); // Sodium nitrate (s)
+		subItems.add(new ItemStack(itemIn, 1, setBit(15, 30, true))); // Ammonia (g)
+		subItems.add(new ItemStack(itemIn, 1, setBit(16, 30, true))); // Salty water (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(17, 30, true))); // Mixture of ammonia and salty water (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(18, 30, true))); // CO2 (g)
+		subItems.add(new ItemStack(itemIn, 1, setBit(19, 30, true))); // Mixture of ammonia, salty water and CO2 (l)
+		subItems.add(new ItemStack(itemIn, 1, setBit(20, 30, true))); // Elemental hydrogen (g)
+		subItems.add(new ItemStack(itemIn, 1, setBit(22, 30, true))); // Elemental nitrogen (g)
+		subItems.add(new ItemStack(itemIn, 1, setBit(22, 30, true))); // Elemental oxygen (g)
 	}
 	
 	@Override
@@ -67,16 +151,49 @@ class TestTube extends BasicItem {
 		String type;
 		switch (setBit(stack.getItemDamage(), 30, false)) {
 			case 0: type = "Empty"; break;
-			case 1: type = "Fresh water with contaminants"; break;
-			case 2: type = "Purified water"; break;
-			case 3: type = "NaCl"; break;
+			case 1: type = "Water with contaminants"; break;
+			case 2: type = "Water"; break;
+			case 3: type = "Table salt"; break;
+			case 4: type = "Nitric acid"; break;
+			case 5: type = "Sulfuric acid"; break;
+			case 6: type = "Nitric acid + Sulfuric acid"; break;
+			case 7: type = "Toluene"; break;
+			case 8: type = "Nitrotoluene"; break;
+			case 9: type = "Sodium bicarbonate"; break;
+			case 10: type = "Cleaned nitrotoluene"; break;
+			case 11: type = "Unfinished TNT"; break;
+			case 12: type = "Raw TNT"; break;
+			case 13: type = "Copper nitrate"; break;
+			case 14: type = "Sodium nitrate"; break;
+			case 15: type = "Ammonia"; break;
+			case 16: type = "Water + Table salt"; break;
+			case 17: type = "Ammonia + Water + Table salt"; break;
+			case 18: type = "Carbon dioxide"; break;
+			case 19: type = "Ammonia + H2O + Table salt + CO2"; break;
+			case 20: type = "Elemental hydrogen"; break;
+			case 21: type = "Elemental nitrogen"; break;
+			case 22: type = "Elemental oxygen"; break;
 			default: type = "Empty"; break;
 		}
 		if (getBit(stack.getItemDamage(), 30) == 1) type = type + " (stopped)";
 		tooltip.add(type);
+		
+		// Warnings
+		String warn;
+		switch (setBit(stack.getItemDamage(), 30, false)) {
+			case 4: warn = "Highly corrosive"; break; // Nitric acid
+			case 5: warn = "Highly corrosive"; break; // Sulfuric acid
+			case 6: warn = "Highly corrosive"; break; // Nitric + sulfuric acid
+			case 7: warn = "Highly flammable"; break; // Toluene
+			case 13: warn = "Explosive"; break; // Raw TNT
+			case 15: warn = "Alkaline"; break; // Ammonia
+			case 20: warn = "Extremely flammable and explosive"; break; // Elemental hydrogen
+			case 22: warn = "Purified oxidizer"; break; // Elemental oxygen
+			default: warn = ""; break;
+		}
+		tooltip.add(warn);
 	}
 	
-	// Code "borrowed" from net.minecraft.item.ItemBucket.
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand) {
 		RayTraceResult raytraceresult = this.rayTrace(world, player, true);
@@ -103,11 +220,11 @@ class TestTube extends BasicItem {
 	}
 	
 	// Convenience method.
-	int getBit(int num, int index) {
+	private int getBit(int num, int index) {
 		return (num >> index) & 1;
 	}
 	
-	int setBit(int num, int index, boolean value) {
+	private int setBit(int num, int index, boolean value) {
 		if (value) {
 			return num | (1 << index);
 		} else {
