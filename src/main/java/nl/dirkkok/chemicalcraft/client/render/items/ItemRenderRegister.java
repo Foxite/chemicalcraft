9ee78515c9031e9d270d623c3a97975460b7b11f
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-27 20:19:21 +0200

    added 20 new test tube contents

--- src/main/java/nl/dirkkok/chemicalcraft/client/render/items/ItemRenderRegister.java
+++ src/main/java/nl/dirkkok/chemicalcraft/client/render/items/ItemRenderRegister.java
@@ -18,11 +18,44 @@ public final class ItemRenderRegister {
 		reg(ModItems.testTube, 0, "test_tube");
 		reg(ModItems.testTube, 1, "test_tube_water");
 		reg(ModItems.testTube, 2, "test_tube_purewater");
-		reg(ModItems.testTube, 3, "test_tube_nacl");
+		reg(ModItems.testTube, 3, "test_tube_table_salt");
+		reg(ModItems.testTube, 4, "test_tube_nitric_acid");
+		reg(ModItems.testTube, 5, "test_tube_sulfuric_acid");
+		reg(ModItems.testTube, 6, "test_tube_nitric+sulfuric_acid");
+		reg(ModItems.testTube, 7, "test_tube_toluene");
+		reg(ModItems.testTube, 8, "test_tube_nitrotoluene");
+		reg(ModItems.testTube, 9, "test_tube_sodium_bicarbonate");
+		reg(ModItems.testTube, 10, "test_tube_cleaned_nitrotoluene");
+		reg(ModItems.testTube, 11, "test_tube_unfinished_tnt");
+		reg(ModItems.testTube, 12, "test_tube_raw_tnt");
+		reg(ModItems.testTube, 13, "test_tube_copper2_nitrate");
+		reg(ModItems.testTube, 14, "test_tube_sodium_nitrate");
+		reg(ModItems.testTube, 16, "test_tube_salty_water");
+		reg(ModItems.testTube, 17, "test_tube_ammonia+salty_water");
+		reg(ModItems.testTube, 19, "test_tube_ammonia+salty_water+co2");
 		reg(ModItems.testTube, (1 << 30), "test_tube_stopped");
 		reg(ModItems.testTube, 1 + (1 << 30), "test_tube_water_stopped");
 		reg(ModItems.testTube, 2 + (1 << 30), "test_tube_purewater_stopped");
-		reg(ModItems.testTube, 3 + (1 << 30), "test_tube_nacl_stopped");
+		reg(ModItems.testTube, 3 + (1 << 30), "test_tube_table_salt_stopped");
+		reg(ModItems.testTube, 4 + (1 << 30), "test_tube_nitric_acid_stopped");
+		reg(ModItems.testTube, 5 + (1 << 30), "test_tube_sulfuric_acid_stopped");
+		reg(ModItems.testTube, 6 + (1 << 30), "test_tube_nitric+sulfuric_acid_stopped");
+		reg(ModItems.testTube, 7 + (1 << 30), "test_tube_toluene_stopped");
+		reg(ModItems.testTube, 8 + (1 << 30), "test_tube_nitrotoluene_stopped");
+		reg(ModItems.testTube, 9 + (1 << 30), "test_tube_sodium_bicarbonate_stopped");
+		reg(ModItems.testTube, 10 + (1 << 30), "test_tube_cleaned_nitrotoluene_stopped");
+		reg(ModItems.testTube, 11 + (1 << 30), "test_tube_unfinished_tnt_stopped");
+		reg(ModItems.testTube, 12 + (1 << 30), "test_tube_raw_tnt_stopped");
+		reg(ModItems.testTube, 13 + (1 << 30), "test_tube_copper2_nitrate_stopped");
+		reg(ModItems.testTube, 14 + (1 << 30), "test_tube_sodium_nitrate_stopped");
+		reg(ModItems.testTube, 15 + (1 << 30), "test_tube_ammonia_stopped");
+		reg(ModItems.testTube, 16 + (1 << 30), "test_tube_salty_water_stopped");
+		reg(ModItems.testTube, 17 + (1 << 30), "test_tube_ammonia+salty_water_stopped");
+		reg(ModItems.testTube, 18 + (1 << 30), "test_tube_co2_stopped");
+		reg(ModItems.testTube, 19 + (1 << 30), "test_tube_ammonia+salty_water+co2_stopped");
+		reg(ModItems.testTube, 20 + (1 << 30), "test_tube_hydrogen_stopped");
+		reg(ModItems.testTube, 21 + (1 << 30), "test_tube_nitrogen_stopped");
+		reg(ModItems.testTube, 22 + (1 << 30), "test_tube_oxygen_stopped");
 	}
 	
 	private static void reg(Item item) {
