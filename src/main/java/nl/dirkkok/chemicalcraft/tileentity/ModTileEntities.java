27150751cf041c2353a577bd610c1e08f06c2ded
Dirk Kok <d.kok.2000@hotmail.nl>
2016-10-31 19:27:14 +0100

    remove commented code

--- src/main/java/nl/dirkkok/chemicalcraft/tileentity/ModTileEntities.java
+++ src/main/java/nl/dirkkok/chemicalcraft/tileentity/ModTileEntities.java
@@ -1,9 +1,14 @@
 package nl.dirkkok.chemicalcraft.tileentity;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import nl.dirkkok.chemicalcraft.ChemicalCraft;
 
 public final class ModTileEntities {
 	public static void init() {
 		GameRegistry.registerTileEntity(ChemistryStandEntity.class, "chemistry_stand_entity");
+		
+		if (ChemicalCraft.supportedModsLoaded.contains("ic2")) {
+			GameRegistry.registerTileEntity(ElectrolysisStandEntity.class, "electrolysis_stand_entity");
+		}
 	}
 }
