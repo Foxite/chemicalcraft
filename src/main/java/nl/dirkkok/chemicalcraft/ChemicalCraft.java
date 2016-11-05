a1ee31a12408151cefa041198379a46e191ade1e
Dirk Kok <d.kok.2000@hotmail.nl>
2016-11-05 16:29:05 +0100

    updated forge

--- src/main/java/nl/dirkkok/chemicalcraft/ChemicalCraft.java
+++ src/main/java/nl/dirkkok/chemicalcraft/ChemicalCraft.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class ChemicalCraft {
 	public static final String MODID = "chemicalcraft";
 	public static final String MODNAME = "ChemicalCraft";
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "1.10.2-0.0.5.0";
 	public static Logger LOG;
 	@SidedProxy(clientSide = "nl.dirkkok.chemicalcraft.ClientProxy",
 				serverSide = "nl.dirkkok.chemicalcraft.ServerProxy")
